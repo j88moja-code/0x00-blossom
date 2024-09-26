0e709f3b885ae3f6adc2bcbf30904cda2b4f01cb
j88moja-code <sitholent+github@gmail.com>
2024-09-26 10:38:07 +0200

    dashboard tabs fix

--- src/routes/_layout/index.lazy.tsx
+++ src/routes/_layout/index.lazy.tsx
@@ -38,25 +38,25 @@ function HomePage() {
             <TabsList className="flex flex-wrap justify-center sm:justify-start gap-2">
               <TabsTrigger
                 value="main"
-                className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 focus:ring-offset-blue-100"
+                // className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 focus:ring-offset-blue-100"
               >
                 Main
               </TabsTrigger>
               <TabsTrigger
                 value="equipment"
-                className="px-4 py-2 text-sm font-medium text-white  focus:ring-green-300 focus:ring-offset-2 focus:ring-offset-green-100"
+                // className="px-4 py-2 text-sm font-medium text-white  focus:ring-green-300 focus:ring-offset-2 focus:ring-offset-green-100"
               >
                 Equipment
               </TabsTrigger>
               <TabsTrigger
                 value="maintenance-requests"
-                className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 focus:ring-offset-yellow-100"
+                // className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 focus:ring-offset-yellow-100"
               >
                 Maintenance Requests
               </TabsTrigger>
               <TabsTrigger
                 value="maintenance-tickets"
-                className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-offset-2 focus:ring-offset-red-100"
+                // className="px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-offset-2 focus:ring-offset-red-100"
               >
                 Maintenance Tickets
               </TabsTrigger>
